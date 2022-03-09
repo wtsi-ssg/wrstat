b96f9dba9358cd9e7044dfe785b5b48b2e482992
Sendu Bala <sb10@sanger.ac.uk>
2022-03-09 16:21:19 +0000

    New server cmd, rename tree cmd to where and dgut.

---
+++ cmd/server.go
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Genome Research Ltd.
+ *
+ * Author: Sendu Bala <sb10@sanger.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/wtsi-ssg/wrstat/server"
+)
+
+// options for this cmd.
+var serverPort string
+var serverCert string
+var serverKey string
+var serverDGUT string
+
+// serverCmd represents the server command.
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start the web server",
+	Long: `Start the web server.
+
+Starting the web server brings up a REST API that can use the given dgut.db 
+(as produced by 'wrstat dgut' during 'wrstat mutli') to answer questions about
+where data is on the disks.
+
+The server must be running for 'wrstat where' calls to succeed.
+
+This command will block forever in the foreground; you can background it with
+ctrl-z; bg.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if serverDGUT == "" {
+			die("you must supply the path to the dgut.db")
+		}
+
+		if serverCert == "" {
+			die("you must supply --cert")
+		}
+
+		if serverKey == "" {
+			die("you must supply --key")
+		}
+
+		s := server.New()
+
+		err := s.LoadDGUTDB(serverDGUT)
+		if err != nil {
+			die("failed to load database: %s", err)
+		}
+
+		err = s.Start("0.0.0.0:"+serverPort, serverCert, serverKey)
+		if err != nil {
+			die("non-graceful stop: %s", err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(serverCmd)
+
+	// flags specific to this sub-command
+	serverCmd.Flags().StringVarP(&serverPort, "port", "p", "80",
+		"port to listen on")
+	serverCmd.Flags().StringVarP(&serverDGUT, "db", "d", "",
+		"path to dgut.db")
+	serverCmd.Flags().StringVarP(&serverCert, "cert", "c", "",
+		"path to certificate file")
+	serverCmd.Flags().StringVarP(&serverKey, "key", "k", "",
+		"path to key file")
+
+	serverCmd.SetHelpFunc(func(command *cobra.Command, strings []string) {
+		hideGlobalFlags(serverCmd, command, strings)
+	})
+}
