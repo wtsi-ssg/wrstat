a939dd2ac9ac61619422f52cddd4fe7e01a9dfbc
michael-grace <michael.dp.grace@outlook.com>
2022-07-06 14:51:25 +0000

    Token Permissions

--- server/auth.go
+++ server/auth.go
@@ -40,7 +40,7 @@ type login struct {
 }
 
 const (
-	tokenDuration    = time.Hour * 8
+	tokenDuration    = time.Hour * 24 * 5
 	userKey          = "user"
 	claimKeyUsername = "Username"
 	claimKeyUIDs     = "UIDs"
