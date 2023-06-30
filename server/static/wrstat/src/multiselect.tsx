6f23a556c8c356c63230a7a5df1b8e50631b9a3f
Michael Woolnough <mw31@sanger.ac.uk>
2023-06-30 15:57:37 +0100

    Add filterable, multi-select input for selecting within a large list.

---
+++ server/static/wrstat/src/multiselect.tsx
@@ -0,0 +1,40 @@
+import {useRef, useState} from "react";
+
+export default ({id, list, onchange}: {id: string; list: string[]; onchange: (list: string[]) => void}) => {
+	const [selected, setSelected] = useState(new Set<string>()),
+	filterRef = useRef<HTMLInputElement>(null),
+	[filter, setFilter] = useState(""),
+	filteredList = list.filter(e => e.includes(filter));
+
+	return <div className="multiInput">
+		<ul>
+			<li><button id={id} onClick={() => filterRef.current?.focus()}>+</button></li>
+			{Array.from(selected).map(e => <li onClick={() => {
+				selected.delete(e);
+				setSelected(new Set(selected));
+				onchange(Array.from(selected));
+			}}>{e}</li>)}
+		</ul>
+		<div>
+			<div>
+				<input ref={filterRef} value={filter} onChange={e => setFilter(e.target.value)} />
+				<ul tabIndex={-1}>
+					{
+						filteredList.map(e => <li>
+							<label><input type="checkbox" checked={selected.has(e)} onChange={() => {
+								if (selected.has(e)) {
+									selected.delete(e);
+								} else {
+									selected.add(e);
+								}
+
+								setSelected(new Set(selected));
+								onchange(Array.from(selected));
+							}} />{e}</label>
+						</li>)
+					}
+				</ul>
+			</div>
+		</div>
+	</div>
+};
