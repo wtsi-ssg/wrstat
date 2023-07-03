075bf76bebeb72645689c8bac7537be9e5ab770e
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-03 11:52:25 +0100

    Centre 'empty dir' text.

--- server/static/wrstat/src/treemap.tsx
+++ server/static/wrstat/src/treemap.tsx
@@ -134,7 +134,7 @@ export default ({table, width, height, onmouseout}: {table: Table | null; width:
 	if (filteredTable.length === 0) {
 		return <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
 			<rect width="100%" height="100%" stroke="#000" fill="#fff" />
-			<text x={width / 2} y={height / 2} >—No Sub-Directories—</text>
+			<text text-anchor="middle" x={width / 2} y={height / 2} >—No Sub-Directories—</text>
 		</svg>;
 	}
 	
