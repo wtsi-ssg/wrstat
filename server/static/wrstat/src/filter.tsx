ee2a5f87b42aa89909825a88ce02367673b16e14
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-05 14:06:55 +0100

    Add scatter plot to filter so it can later be used to filter the basedirs table on time and space.

--- server/static/wrstat/src/filter.tsx
+++ server/static/wrstat/src/filter.tsx
@@ -2,15 +2,18 @@ import type {Usage} from "./rpc";
 import {useState} from "react";
 import FilteredTable from "./filteredTable";
 import MultiSelect from "./multiselect";
+import Scatter from "./scatter";
 
 const stringSort = new Intl.Collator().compare;
 
 export default ({groupUsage, userUsage, areas /*, history*/}: {groupUsage: Usage[], userUsage: Usage[], areas: Record<string, string[]> /*, history: Map<string, History[]>*/}) => {
-	const [byUser, setBy] = useState(false);
-	const [users, setUsers] = useState<string[]>([]);
-	const [groups, setGroups] = useState<string[]>([]);
-	const [boms, setBOMs] = useState<string[]>([]);
-	const [owners, setOwners] = useState<string[]>([]);
+	const [byUser, setBy] = useState(false),
+	[users, setUsers] = useState<string[]>([]),
+	[groups, setGroups] = useState<string[]>([]),
+	[boms, setBOMs] = useState<string[]>([]),
+	[owners, setOwners] = useState<string[]>([]),
+	[scaleSize, setScaleSize] = useState(false),
+	[scaleDays, setScaleDays] = useState(false);
 
 	return <>
 		<div className="treeFilter">
@@ -18,7 +21,7 @@ export default ({groupUsage, userUsage, areas /*, history*/}: {groupUsage: Usage
 			<input type="radio" name="by" id="byGroup" checked={!byUser} onChange={e => setBy(!e.target.checked)} />
 			<label htmlFor="byUser">By User</label>
 			<input type="radio" name="by" id="byUser" checked={byUser} onChange={e => setBy(e.target.checked)} />
-			<br />
+			<Scatter width={900} height={400} data={byUser ? userUsage : groupUsage} logX={scaleDays} logY={scaleSize} />
 			{!byUser ? [] : <>
 				<label htmlFor="username">Username</label>
 				<MultiSelect id="username" list={Array.from(new Set(userUsage.map(e => e.Name)).values()).sort(stringSort)} onchange={setUsers} />
@@ -33,6 +36,10 @@ export default ({groupUsage, userUsage, areas /*, history*/}: {groupUsage: Usage
 				<label htmlFor="bom">Group Areas</label>
 				<MultiSelect id="bom" list={Object.keys(areas).sort(stringSort)} onchange={setBOMs} />
 			</>}
+			<label htmlFor="scaleSize">Log Size</label>
+			<input type="checkbox" id="scaleSize" checked={scaleSize} onChange={e => setScaleSize(e.target.checked)} />
+			<label htmlFor="scaleDays">Log Days</label>
+			<input type="checkbox" id="scaleDays" checked={scaleDays} onChange={e => setScaleDays(e.target.checked)} />
 		</div>
 		<FilteredTable usage={byUser ? userUsage : groupUsage} byUser={byUser} name={byUser ? users : groups.concat(boms.map(b => areas[b]).flat())} owner={owners} /*history={history}*/ />
 	</>
