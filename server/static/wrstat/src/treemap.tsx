e2a0c6ccffb4e37f10dca11733ebd894727fb394
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-12 09:27:43 +0100

    Replace old lock icon with new one on 'Not Authorised' view.

--- server/static/wrstat/src/treemap.tsx
+++ server/static/wrstat/src/treemap.tsx
@@ -72,7 +72,7 @@ const phi = (1 + Math.sqrt(5)) / 2,
 				d += isRow ? colWidth : rowHeight;
 
 				toRet.push(
-					<rect key={entry.key ? `rect_${entry.key}` : ""} x={left} y={top} width={colWidth} height={rowHeight} stroke="#000" fill={entry.backgroundColour ?? "#fff"} className={entry.onclick ? "hasClick" : ""} onClick={entry.onclick} onMouseOver={entry.onmouseover}><title>{entry.name}</title></rect>,
+					<rect className="box" key={entry.key ? `rect_${entry.key}` : ""} x={left} y={top} width={colWidth} height={rowHeight} stroke="#000" fill={entry.backgroundColour ?? "#fff"} className={entry.onclick ? "hasClick" : ""} onClick={entry.onclick} onMouseOver={entry.onmouseover}><title>{entry.name}</title></rect>,
 					entry.noauth ? <use key={entry.key ? `lock_${entry.key}` : ""} x={left + (colWidth - bbox.width * minScale + minScale * 0.45) / 2} y={top + (rowHeight - minScale * 0.40) / 2} href="#lock" width="0.5em" height="0.5em" style={{ fontSize: `${minScale * 0.9}px` }} /> : <></>,
 					<text key={entry.key ? `text_${entry.key}` : ""} fontSize={minScale * 0.9} fontFamily={font} x={(entry.noauth ? minScale * 0.225 : 0) + left + colWidth / 2} y={top + rowHeight / 2 + 0.225 * minScale * bbox.height} textAnchor="middle" fill={entry.colour ?? "#000"}>{entry.name}</text>
 				);
@@ -140,10 +140,9 @@ export default ({ table, width, height, noAuth = false, onmouseout }: { table: T
 			{
 				noAuth ?
 					<>
-						<svg height={150} transform={`translate(0 ${((height / 2) - 150) / 2})`} viewBox="0 0 70 100" style={{ stroke: "#000" }} stroke-width={2} stroke-linejoin="round">
-							<path d="M15,45 v-20 a1,1 0,0,1 40,0 v20 h-10 v-20 a1,1 0,0,0 -20,0 v20 z" fill="#ccc" />
-							<rect x={5} y={45} width={60} height={50} fill="#aaa" stroke-width={4} rx={10} />
-							<path d="M30,78 l2,-8 c-7,-12 13,-12 6,0 l2,8 z" fill="#666" stroke="#000" stroke-linejoin="round" />
+						<svg height={150} transform={`translate(0 ${((height / 2) - 200) / 2})`} viewBox="0 0 100 100" style={{ stroke: "#000" }} stroke-width={2} stroke-linejoin="round">
+							<rect rx="15" x="5" y="38" width="90" height="62" fill="#000" />
+							<path d="M27,40 v-10 a1,1 0,0,1 46,0 v10" fill="none" stroke="#000" stroke-width="12" />
 						</svg>
 						<text text-anchor="middle" x={width / 2} y={height / 2} >—Not Authorised—</text>
 					</> :
