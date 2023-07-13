d8020457227e472b8abcc199bc3c0fbbd9d6cbe6
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-13 09:04:33 +0100

    Add missing semi-colon.

--- server/static/wrstat/src/history.tsx
+++ server/static/wrstat/src/history.tsx
@@ -33,7 +33,7 @@ const HistoryComponent = ({ id, path, name, owner, isUser }: { id: number; path:
 		<HistoryGraph history={history.map(h => ({ Date: h.Date, Usage: inodeHistory ? h.UsageInodes : h.UsageSize, Quota: inodeHistory ? h.QuotaInodes : h.QuotaSize }))} width={960} height={500} yFormatter={inodeHistory ? formatLargeNumber : formatBytes} secondaryFormatter={inodeHistory ? formatNumber : (num: number) => formatNumber(num) + " Bytes"} yRounder={inodeHistory ? (maxAmount: number) => {
 			const order = Math.pow(10, Math.max(Math.floor(Math.log10(maxAmount)), 1));
 
-			return maxAmount = order * Math.ceil(maxAmount / order)
+			return maxAmount = order * Math.ceil(maxAmount / order);
 		} : (maxAmount: number) => 100 * Math.pow(2, Math.ceil(Math.log2(maxAmount / 100)))} />
 		{
 			!inodeHistory && exceedSize === 0 ? <div className="exceeded">Size Quota has been reached.</div> :
