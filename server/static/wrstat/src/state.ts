f5b91991198d657f09fa863214d591b7af88477f
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-18 16:22:31 +0100

    Reset state module to original.

--- server/static/wrstat/src/state.ts
+++ server/static/wrstat/src/state.ts
@@ -1,39 +1,4 @@
 import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
-
-export function useSearchParamsState<T>(
-	searchParamName: string,
-	defaultValue: T
-): readonly [
-	searchParamsState: T,
-	setSearchParamsState: (newState: T) => void
-] {
-	const [searchParams, setSearchParams] = useSearchParams();
-
-	const acquiredSearchParamStr = searchParams.get(searchParamName);
-	let searchParamsState = defaultValue
-	if (acquiredSearchParamStr !== null) {
-		let d = decodeURIComponent(acquiredSearchParamStr);
-		console.log('%s => %s', acquiredSearchParamStr, d);
-		searchParamsState = JSON.parse(d) as T
-	}
-
-	const setSearchParamsState = (newState: T) => {
-		const serialized = encodeURIComponent(JSON.stringify(newState))
-		console.log('serialzed key %s val %s to %s', searchParamName, newState, serialized)
-		const next = Object.assign(
-			{},
-			[...searchParams.entries()].reduce(
-				(o, [key, value]) => ({ ...o, [key]: value }),
-				{}
-			),
-			{ [searchParamName]: serialized }
-		);
-		setSearchParams(next);
-		console.log('setSerachParams(%s)', JSON.stringify(next))
-	};
-	return [searchParamsState, setSearchParamsState];
-}
 
 const state = new Map<string, any>(),
 	isDefaultValue = <T>(v: T, def: T) => v === def || JSON.stringify(v) === JSON.stringify(def),
@@ -133,44 +98,3 @@ export const useSavedState = <T>(name: string, v: T) => {
 			fn(v);
 		}
 	};
-
-// import { useSearchParams } from "react-router-dom";
-
-// export default function useURLState<T>(
-//     searchParamName: string,
-//     defaultValue: T
-// ): readonly [
-//     searchParamsState: T,
-//     setSearchParamsState: (newState: T) => void
-// ] {
-//     const [searchParams, setSearchParams] = useSearchParams();
-
-//     const acquiredSearchParamStr = searchParams.get(searchParamName);
-//     let searchParamsState = defaultValue
-//     if (acquiredSearchParamStr !== null) {
-//         let d = decodeURIComponent(acquiredSearchParamStr);
-//         console.log('%s => %s', acquiredSearchParamStr, d);
-//         searchParamsState = JSON.parse(d) as T
-//     }
-
-//     const setSearchParamsState = (newState: T) => {
-//         const serialized = encodeURIComponent(JSON.stringify(newState))
-//         console.log('serialzed key %s val %s to %s', searchParamName, newState, serialized)
-//         const next = Object.assign(
-//             {},
-//             [...searchParams.entries()].reduce(
-//                 (o, [key, value]) => ({ ...o, [key]: value }),
-//                 {}
-//             ),
-//             { [searchParamName]: serialized }
-//         );
-//         setSearchParams(next);
-//         console.log('setSerachParams(%s)', JSON.stringify(next))
-//     };
-//     return [searchParamsState, setSearchParamsState];
-// }
-
-// export function useClearState() {
-//     const [_, setSearchParams] = useSearchParams();
-//     setSearchParams(undefined)
-// }
