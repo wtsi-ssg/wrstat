e04465c5cf4ee8d1f95242a96e6787505643b963
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-19 09:43:33 +0100

    Allow keyboard navigation for tabs.

--- server/static/wrstat/src/Tabs.tsx
+++ server/static/wrstat/src/Tabs.tsx
@@ -6,8 +6,14 @@ type TabData = {
 
 const Tabs = ({ id, tabs }: { id: string, tabs: TabData[] }) => <ul id={id} className="tabs">
 	{tabs.map(tab => <li
+		tabIndex={0}
 		className={tab.selected ? "selected" : ""}
 		onClick={tab.selected ? undefined : () => tab.onClick(tab.title)}
+		onKeyDown={tab.selected ? undefined : e => {
+			if (e.key === "Enter") {
+				tab.onClick(tab.title);
+			}
+		}}
 	>{tab.title}</li>)}
 </ul>
 
