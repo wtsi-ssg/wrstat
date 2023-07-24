958c0bf1617c51d464a5dbe16a933ed4d0daa15b
Michael Woolnough <mw31@sanger.ac.uk>
2023-07-24 09:26:09 +0100

    Move graph key to be in line with top of graph.

--- server/static/wrstat/src/HistoryGraph.tsx
+++ server/static/wrstat/src/HistoryGraph.tsx
@@ -175,11 +175,11 @@ const paddingXL = 80,
 				{
 					y === latestHistory.Quota ? <path d="M5,5 L-5,-5 M-5,5 L5,-5" stroke="#f00" stroke-width={2} transform={`translate(${paddingXL + (x - minDate) * xScale} ${amountToY(y)})`} onMouseOver={() => setInfoBox(infoBoxes.length - 1)} onMouseOut={() => setInfoBox(-1)} /> : []
 				}
-				<rect rx={4} x={width - 70} y={paddingYT + 10} width={69} height={43} stroke="currentColor" fill="none" />
-				<rect rx={2} x={width - 65} y={paddingYT + 15} width={10} height={10} stroke="currentColor" style={{ fill: "var(--graphQuota)" }} />
-				<text x={width - 48} y={paddingYT + 25} fill="currentColor">Quota</text>
-				<rect rx={2} x={width - 65} y={paddingYT + 35} width={10} height={10} stroke="currentColor" style={{ fill: "var(--graphUsage)" }} />
-				<text x={width - 48} y={paddingYT + 45} fill="currentColor">Usage</text>
+				<rect rx={4} x={width - 70} y={paddingYT} width={69} height={43} stroke="currentColor" fill="none" />
+				<rect rx={2} x={width - 65} y={paddingYT + 5} width={10} height={10} stroke="currentColor" style={{ fill: "var(--graphQuota)" }} />
+				<text x={width - 48} y={paddingYT + 15} fill="currentColor">Quota</text>
+				<rect rx={2} x={width - 65} y={paddingYT + 25} width={10} height={10} stroke="currentColor" style={{ fill: "var(--graphUsage)" }} />
+				<text x={width - 48} y={paddingYT + 35} fill="currentColor">Usage</text>
 			</svg>
 		</>
 	};
