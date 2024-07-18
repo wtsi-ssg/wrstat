f72aeee26d60a4a861a16e67b1aff3d9bfe1fd83
Michael Woolnough <mw31@sanger.ac.uk>
2024-07-18 16:39:43 +0100

    Disable job submission to WR.

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -14,13 +16,47 @@ import (
 const app = "wrstat"
 
 func buildSelf() func() {
+	f, err := os.Open("cmd/root.go")
+	if err != nil {
+		failMainTest(err.Error())
+	}
+
+	code, err := io.ReadAll(f)
+	if err != nil {
+		failMainTest(err.Error())
+	}
+
+	f.Close()
+
+	f, err = os.Create("cmd/root.go")
+	if err != nil {
+		failMainTest(err.Error())
+	}
+
+	_, err = f.Write(bytes.Replace(
+		code,
+		[]byte("func addJobsToQueue(s *scheduler.Scheduler, jobs []*jobqueue.Job) {"),
+		[]byte("func addJobsToQueue(s *scheduler.Scheduler, jobs []*jobqueue.Job) {}\nfunc NOaddJobsToQueue(s *scheduler.Scheduler, jobs []*jobqueue.Job) {"),
+		1,
+	))
+	if err != nil {
+		failMainTest(err.Error())
+	}
+
+	f.Close()
+
 	if err := exec.Command("make", "buildnonpm").Run(); err != nil {
 		failMainTest(err.Error())
 
 		return nil
 	}
 
-	return func() { os.Remove(app) }
+	return func() {
+		os.Remove(app)
+		f, _ = os.Create("cmd/root.go")
+		f.Write(code)
+		f.Close()
+	}
 }
 
 func failMainTest(err string) {
@@ -38,10 +74,10 @@ func TestMain(m *testing.M) {
 }
 
 func TestVersion(t *testing.T) {
-	Convey("", t, func() {
-		cd := exec.Command("./wrstat", "version")
+	Convey("wrstat prints the correct version", t, func() {
+		run := exec.Command("./wrstat", "version")
 
-		output, err := cd.CombinedOutput()
+		output, err := run.CombinedOutput()
 		So(err, ShouldBeNil)
 		So(strings.TrimSpace(string(output)), ShouldEqual, cmd.Version)
 	})
