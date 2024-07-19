cb9b4c634ead996dd1b3026962ac6ddf8069055f
Michael Woolnough <mw31@sanger.ac.uk>
2024-07-19 10:06:11 +0100

    Add test for Stat cmd.

--- main_test.go
+++ main_test.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 	"strings"
+	"syscall"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -54,5 +61,148 @@ func TestVersion(t *testing.T) {
 		output, err := cmd.CombinedOutput()
 		So(err, ShouldBeNil)
 		So(strings.TrimSpace(string(output)), ShouldEqual, "TESTVERSION")
+	})
+}
+
+func TestStat(t *testing.T) {
+	type File struct {
+		name   string
+		length int
+		mtime  time.Time
+	}
+
+	tmp := t.TempDir()
+
+	Convey("Given a valid walk file, the stats file prints the correct output", t, func() {
+		var (
+			inodes         []uint64
+			dev            uint64
+			atimes, ctimes []int64
+		)
+
+		for _, stats := range [...]File{
+			{
+				name:   "aDirectory/aFile",
+				mtime:  time.Unix(7383773, 0),
+				length: 10,
+			},
+			{
+				name:  "aDirectory/aSubDirectory",
+				mtime: time.Unix(314159, 0),
+			},
+			{
+				name:  "aDirectory",
+				mtime: time.Unix(133032, 0),
+			},
+			{
+				name:  "anotherDirectory",
+				mtime: time.Unix(282820, 0),
+			},
+			{
+				name:  ".",
+				mtime: time.Unix(271828, 0),
+			},
+		} {
+			path := filepath.Join(tmp, stats.name)
+
+			if stats.length > 0 {
+				err := os.MkdirAll(filepath.Dir(path), 0755)
+				So(err, ShouldBeNil)
+
+				f, err := os.Create(path)
+				So(err, ShouldBeNil)
+
+				_, err = f.Write(make([]byte, stats.length))
+				So(err, ShouldBeNil)
+
+				err = f.Close()
+				So(err, ShouldBeNil)
+			} else {
+				err := os.MkdirAll(path, 0755)
+				So(err, ShouldBeNil)
+			}
+
+			stat, err := os.Stat(path)
+			So(err, ShouldBeNil)
+
+			statt, ok := stat.Sys().(*syscall.Stat_t)
+			So(ok, ShouldBeTrue)
+
+			inodes = append(inodes, statt.Ino)
+			dev = statt.Dev
+			atimes = append(atimes, statt.Atim.Sec)
+			ctimes = append(ctimes, statt.Ctim.Sec)
+
+			err = os.Chtimes(path, time.Time{}, stats.mtime)
+			So(err, ShouldBeNil)
+		}
+
+		statDir := t.TempDir()
+		statFilePath := filepath.Join(statDir, "dir.walk")
+		statFile, err := os.Create(statFilePath)
+		So(err, ShouldBeNil)
+
+		err = fs.WalkDir(os.DirFS(tmp), ".", func(path string, _ fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			_, err = io.WriteString(statFile, filepath.Join(tmp, path)+"\n")
+			So(err, ShouldBeNil)
+
+			return nil
+		})
+		So(err, ShouldBeNil)
+
+		err = statFile.Close()
+		So(err, ShouldBeNil)
+
+		cmd := exec.Command("./wrstat", "stat", statFilePath)
+		err = cmd.Run()
+
+		So(err, ShouldBeNil)
+
+		u, err := user.Current()
+		So(err, ShouldBeNil)
+
+		expectation := fmt.Sprintf(""+
+			"%[3]s\t4096\t%[1]s\t%[2]s\t%[14]d\t271828\t%[19]d\td\t%[8]d\t4\t%[13]d\n"+
+			"%[4]s\t4096\t%[1]s\t%[2]s\t%[15]d\t133032\t%[20]d\td\t%[9]d\t3\t%[13]d\n"+
+			"%[5]s\t10\t%[1]s\t%[2]s\t%[16]d\t7383773\t%[21]d\tf\t%[10]d\t1\t%[13]d\n"+
+			"%[6]s\t4096\t%[1]s\t%[2]s\t%[17]d\t314159\t%[22]d\td\t%[11]d\t2\t%[13]d\n"+
+			"%[7]s\t4096\t%[1]s\t%[2]s\t%[18]d\t282820\t%[23]d\td\t%[12]d\t2\t%[13]d\n",
+			u.Uid,
+			u.Gid,
+			base64.StdEncoding.EncodeToString([]byte(tmp)),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aFile"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aSubDirectory"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "anotherDirectory"))),
+			inodes[4],
+			inodes[2],
+			inodes[0],
+			inodes[1],
+			inodes[3],
+			dev,
+			atimes[4],
+			atimes[3],
+			atimes[2],
+			atimes[0],
+			atimes[1],
+			ctimes[4],
+			ctimes[3],
+			ctimes[2],
+			ctimes[0],
+			ctimes[1],
+		)
+
+		f, err := os.Open(filepath.Join(statDir, "dir.walk.stats"))
+		So(err, ShouldBeNil)
+
+		data, err := io.ReadAll(f)
+		f.Close()
+		So(err, ShouldBeNil)
+
+		So(string(data), ShouldEqual, expectation)
 	})
 }
