90ca47c3a9b3887318e2ec17e78d79290dba5694
Michael Woolnough <mw31@sanger.ac.uk>
2024-07-24 14:54:45 +0100

    Re-order tests to be in usage order (order cron would call them).

--- main_test.go
+++ main_test.go
@@ -102,292 +102,6 @@ func TestVersion(t *testing.T) {
 	})
 }
 
-func TestStat(t *testing.T) {
-	type File struct {
-		name   string
-		length int
-		mtime  time.Time
-	}
-
-	tmp := t.TempDir()
-
-	Convey("Given a valid walk file, the stats file prints the correct output", t, func() {
-		var (
-			inodes         []uint64
-			dev            uint64
-			atimes, ctimes []int64
-		)
-
-		for _, stats := range [...]File{
-			{
-				name:   "aDirectory/aFile",
-				mtime:  time.Unix(7383773, 0),
-				length: 10,
-			},
-			{
-				name:  "aDirectory/aSubDirectory",
-				mtime: time.Unix(314159, 0),
-			},
-			{
-				name:  "aDirectory",
-				mtime: time.Unix(133032, 0),
-			},
-			{
-				name:  "anotherDirectory",
-				mtime: time.Unix(282820, 0),
-			},
-			{
-				name:  ".",
-				mtime: time.Unix(271828, 0),
-			},
-		} {
-			path := filepath.Join(tmp, stats.name)
-
-			if stats.length > 0 {
-				err := os.MkdirAll(filepath.Dir(path), 0755)
-				So(err, ShouldBeNil)
-
-				f, err := os.Create(path)
-				So(err, ShouldBeNil)
-
-				_, err = f.Write(make([]byte, stats.length))
-				So(err, ShouldBeNil)
-
-				err = f.Close()
-				So(err, ShouldBeNil)
-			} else {
-				err := os.MkdirAll(path, 0755)
-				So(err, ShouldBeNil)
-			}
-
-			stat, err := os.Stat(path)
-			So(err, ShouldBeNil)
-
-			statt, ok := stat.Sys().(*syscall.Stat_t)
-			So(ok, ShouldBeTrue)
-
-			inodes = append(inodes, statt.Ino)
-			dev = statt.Dev
-			atimes = append(atimes, statt.Atim.Sec)
-			ctimes = append(ctimes, statt.Ctim.Sec)
-
-			err = os.Chtimes(path, time.Time{}, stats.mtime)
-			So(err, ShouldBeNil)
-		}
-
-		statDir := t.TempDir()
-		statFilePath := filepath.Join(statDir, "dir.walk")
-		statFile, err := os.Create(statFilePath)
-		So(err, ShouldBeNil)
-
-		err = fs.WalkDir(os.DirFS(tmp), ".", func(path string, _ fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-
-			_, err = io.WriteString(statFile, filepath.Join(tmp, path)+"\n")
-			So(err, ShouldBeNil)
-
-			return nil
-		})
-		So(err, ShouldBeNil)
-
-		err = statFile.Close()
-		So(err, ShouldBeNil)
-
-		_, _, _, err = runWRStat("stat", statFilePath)
-
-		So(err, ShouldBeNil)
-
-		u, err := user.Current()
-		So(err, ShouldBeNil)
-
-		expectation := fmt.Sprintf(""+
-			"%[3]s\t4096\t%[1]s\t%[2]s\t%[14]d\t271828\t%[19]d\td\t%[8]d\t4\t%[13]d\n"+
-			"%[4]s\t4096\t%[1]s\t%[2]s\t%[15]d\t133032\t%[20]d\td\t%[9]d\t3\t%[13]d\n"+
-			"%[5]s\t10\t%[1]s\t%[2]s\t%[16]d\t7383773\t%[21]d\tf\t%[10]d\t1\t%[13]d\n"+
-			"%[6]s\t4096\t%[1]s\t%[2]s\t%[17]d\t314159\t%[22]d\td\t%[11]d\t2\t%[13]d\n"+
-			"%[7]s\t4096\t%[1]s\t%[2]s\t%[18]d\t282820\t%[23]d\td\t%[12]d\t2\t%[13]d\n",
-			u.Uid,
-			u.Gid,
-			base64.StdEncoding.EncodeToString([]byte(tmp)),
-			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory"))),
-			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aFile"))),
-			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aSubDirectory"))),
-			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "anotherDirectory"))),
-			inodes[4],
-			inodes[2],
-			inodes[0],
-			inodes[1],
-			inodes[3],
-			dev,
-			atimes[4],
-			atimes[3],
-			atimes[2],
-			atimes[0],
-			atimes[1],
-			ctimes[4],
-			ctimes[3],
-			ctimes[2],
-			ctimes[0],
-			ctimes[1],
-		)
-
-		f, err := os.Open(filepath.Join(statDir, "dir.walk.stats"))
-		So(err, ShouldBeNil)
-
-		data, err := io.ReadAll(f)
-		f.Close()
-		So(err, ShouldBeNil)
-
-		So(string(data), ShouldEqual, expectation)
-	})
-}
-
-func TestWalk(t *testing.T) {
-	//TODO: Seems to be sporadically inconsistant, possible race issue.
-
-	Convey("wrstat prints the correct output for a directory", t, func() {
-		out := t.TempDir()
-		tmp := t.TempDir()
-
-		for _, dir := range [...]string{"/a/b/c/d/e", "/a/b/f", "/a/g/h"} {
-			err := os.MkdirAll(filepath.Join(tmp, dir), 0755)
-			So(err, ShouldBeNil)
-		}
-
-		for _, file := range [...]string{"/a/b/c/test.txt", "/a/b/f/test2.csv", "/a/test3"} {
-			f, err := os.Create(filepath.Join(tmp, file))
-			So(err, ShouldBeNil)
-			err = f.Close()
-			So(err, ShouldBeNil)
-		}
-
-		depgroup := "test-group"
-
-		_, _, jobs, err := runWRStat("walk", tmp, "-o", out, "-d", depgroup, "-j", "1")
-		So(err, ShouldBeNil)
-
-		walk1 := filepath.Join(out, "walk.1")
-
-		jobsExpectation := []*jobqueue.Job{
-			{
-				Cmd:        " stat " + walk1,
-				CwdMatters: true,
-				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
-				ReqGroup:   "wrstat-stat",
-				Requirements: &scheduler.Requirements{
-					RAM:   750,
-					Time:  12 * time.Hour,
-					Cores: 1,
-					Disk:  1,
-				},
-				Override:  1,
-				Retries:   30,
-				DepGroups: []string{depgroup},
-			},
-		}
-
-		removeJobRepGroupSuffixes(jobs)
-
-		So(jobs, ShouldResemble, jobsExpectation)
-
-		compareFileContents(t, walk1, fmt.Sprintf(`%[1]s
-%[1]s/a/test3
-%[1]s/a
-%[1]s/a/g
-%[1]s/a/g/h
-%[1]s/a/b
-%[1]s/a/b/f/test2.csv
-%[1]s/a/b/f
-%[1]s/a/b/c/test.txt
-%[1]s/a/b/c
-%[1]s/a/b/c/d
-%[1]s/a/b/c/d/e`, tmp))
-
-		_, _, jobs, err = runWRStat("walk", tmp, "-o", out, "-d", depgroup, "-j", "2")
-		So(err, ShouldBeNil)
-
-		walk2 := filepath.Join(out, "walk.2")
-
-		jobsExpectation = []*jobqueue.Job{
-			{
-				Cmd:        " stat " + walk1,
-				CwdMatters: true,
-				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
-				ReqGroup:   "wrstat-stat",
-				Requirements: &scheduler.Requirements{
-					RAM:   750,
-					Time:  12 * time.Hour,
-					Cores: 1,
-					Disk:  1,
-				},
-				Override:  1,
-				Retries:   30,
-				DepGroups: []string{depgroup},
-			},
-			{
-				Cmd:        " stat " + walk2,
-				CwdMatters: true,
-				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
-				ReqGroup:   "wrstat-stat",
-				Requirements: &scheduler.Requirements{
-					RAM:   750,
-					Time:  12 * time.Hour,
-					Cores: 1,
-					Disk:  1,
-				},
-				Override:  1,
-				Retries:   30,
-				DepGroups: []string{depgroup},
-			},
-		}
-
-		removeJobRepGroupSuffixes(jobs)
-
-		So(jobs, ShouldResemble, jobsExpectation)
-
-		compareFileContents(t, filepath.Join(out, "walk.1"), fmt.Sprintf(`%[1]s
-%[1]s/a
-%[1]s/a/g/h
-%[1]s/a/b/f/test2.csv
-%[1]s/a/b/c/test.txt
-%[1]s/a/b/c/d`, tmp))
-
-		compareFileContents(t, filepath.Join(out, "walk.2"), fmt.Sprintf(`%[1]s/a/test3
-%[1]s/a/g
-%[1]s/a/b
-%[1]s/a/b/f
-%[1]s/a/b/c
-%[1]s/a/b/c/d/e`, tmp))
-	})
-}
-
-func compareFileContents(t *testing.T, filename, expectation string) {
-	t.Helper()
-
-	f, err := os.Open(filename)
-	So(err, ShouldBeNil)
-
-	defer f.Close()
-
-	output, err := io.ReadAll(f)
-	So(err, ShouldBeNil)
-	So(strings.TrimSpace(string(output)), ShouldEqual, expectation)
-}
-
-func removeJobRepGroupSuffixes(jobs []*jobqueue.Job) {
-	for _, job := range jobs {
-		job.RepGroup = job.RepGroup[:len(job.RepGroup)-21]
-	}
-}
-
-func TestMulti(t *testing.T) {
-	Convey("For the multi subcommand", t, func() {
-		multiTests(t, "multi")
-	})
-}
-
 func TestCron(t *testing.T) {
 	Convey("For the cron subcommand", t, func() {
 		multiTests(t, "cron", "-c", "* * * * * *")
@@ -731,6 +445,290 @@ func multiTests(t *testing.T, subcommand ...string) {
 	})
 }
 
+func TestMulti(t *testing.T) {
+	Convey("For the multi subcommand", t, func() {
+		multiTests(t, "multi")
+	})
+}
+
+func TestWalk(t *testing.T) {
+	Convey("wrstat prints the correct output for a directory", t, func() {
+		out := t.TempDir()
+		tmp := t.TempDir()
+
+		for _, dir := range [...]string{"/a/b/c/d/e", "/a/b/f", "/a/g/h"} {
+			err := os.MkdirAll(filepath.Join(tmp, dir), 0755)
+			So(err, ShouldBeNil)
+		}
+
+		for _, file := range [...]string{"/a/b/c/test.txt", "/a/b/f/test2.csv", "/a/test3"} {
+			f, err := os.Create(filepath.Join(tmp, file))
+			So(err, ShouldBeNil)
+			err = f.Close()
+			So(err, ShouldBeNil)
+		}
+
+		depgroup := "test-group"
+
+		_, _, jobs, err := runWRStat("walk", tmp, "-o", out, "-d", depgroup, "-j", "1")
+		So(err, ShouldBeNil)
+
+		walk1 := filepath.Join(out, "walk.1")
+
+		jobsExpectation := []*jobqueue.Job{
+			{
+				Cmd:        " stat " + walk1,
+				CwdMatters: true,
+				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
+				ReqGroup:   "wrstat-stat",
+				Requirements: &scheduler.Requirements{
+					RAM:   750,
+					Time:  12 * time.Hour,
+					Cores: 1,
+					Disk:  1,
+				},
+				Override:  1,
+				Retries:   30,
+				DepGroups: []string{depgroup},
+			},
+		}
+
+		removeJobRepGroupSuffixes(jobs)
+
+		So(jobs, ShouldResemble, jobsExpectation)
+
+		compareFileContents(t, walk1, fmt.Sprintf(`%[1]s
+%[1]s/a/test3
+%[1]s/a
+%[1]s/a/g
+%[1]s/a/g/h
+%[1]s/a/b
+%[1]s/a/b/f/test2.csv
+%[1]s/a/b/f
+%[1]s/a/b/c/test.txt
+%[1]s/a/b/c
+%[1]s/a/b/c/d
+%[1]s/a/b/c/d/e`, tmp))
+
+		_, _, jobs, err = runWRStat("walk", tmp, "-o", out, "-d", depgroup, "-j", "2")
+		So(err, ShouldBeNil)
+
+		walk2 := filepath.Join(out, "walk.2")
+
+		jobsExpectation = []*jobqueue.Job{
+			{
+				Cmd:        " stat " + walk1,
+				CwdMatters: true,
+				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
+				ReqGroup:   "wrstat-stat",
+				Requirements: &scheduler.Requirements{
+					RAM:   750,
+					Time:  12 * time.Hour,
+					Cores: 1,
+					Disk:  1,
+				},
+				Override:  1,
+				Retries:   30,
+				DepGroups: []string{depgroup},
+			},
+			{
+				Cmd:        " stat " + walk2,
+				CwdMatters: true,
+				RepGroup:   "wrstat-stat-" + filepath.Base(tmp) + "-" + time.Now().Format("20060102"),
+				ReqGroup:   "wrstat-stat",
+				Requirements: &scheduler.Requirements{
+					RAM:   750,
+					Time:  12 * time.Hour,
+					Cores: 1,
+					Disk:  1,
+				},
+				Override:  1,
+				Retries:   30,
+				DepGroups: []string{depgroup},
+			},
+		}
+
+		removeJobRepGroupSuffixes(jobs)
+
+		So(jobs, ShouldResemble, jobsExpectation)
+
+		compareFileContents(t, filepath.Join(out, "walk.1"), fmt.Sprintf(`%[1]s
+%[1]s/a
+%[1]s/a/g/h
+%[1]s/a/b/f/test2.csv
+%[1]s/a/b/c/test.txt
+%[1]s/a/b/c/d`, tmp))
+
+		compareFileContents(t, filepath.Join(out, "walk.2"), fmt.Sprintf(`%[1]s/a/test3
+%[1]s/a/g
+%[1]s/a/b
+%[1]s/a/b/f
+%[1]s/a/b/c
+%[1]s/a/b/c/d/e`, tmp))
+	})
+}
+
+func compareFileContents(t *testing.T, filename, expectation string) {
+	t.Helper()
+
+	f, err := os.Open(filename)
+	So(err, ShouldBeNil)
+
+	defer f.Close()
+
+	output, err := io.ReadAll(f)
+	So(err, ShouldBeNil)
+	So(strings.TrimSpace(string(output)), ShouldEqual, expectation)
+}
+
+func removeJobRepGroupSuffixes(jobs []*jobqueue.Job) {
+	for _, job := range jobs {
+		job.RepGroup = job.RepGroup[:len(job.RepGroup)-21]
+	}
+}
+
+func TestStat(t *testing.T) {
+	type File struct {
+		name   string
+		length int
+		mtime  time.Time
+	}
+
+	tmp := t.TempDir()
+
+	Convey("Given a valid walk file, the stats file prints the correct output", t, func() {
+		var (
+			inodes         []uint64
+			dev            uint64
+			atimes, ctimes []int64
+		)
+
+		for _, stats := range [...]File{
+			{
+				name:   "aDirectory/aFile",
+				mtime:  time.Unix(7383773, 0),
+				length: 10,
+			},
+			{
+				name:  "aDirectory/aSubDirectory",
+				mtime: time.Unix(314159, 0),
+			},
+			{
+				name:  "aDirectory",
+				mtime: time.Unix(133032, 0),
+			},
+			{
+				name:  "anotherDirectory",
+				mtime: time.Unix(282820, 0),
+			},
+			{
+				name:  ".",
+				mtime: time.Unix(271828, 0),
+			},
+		} {
+			path := filepath.Join(tmp, stats.name)
+
+			if stats.length > 0 {
+				err := os.MkdirAll(filepath.Dir(path), 0755)
+				So(err, ShouldBeNil)
+
+				f, err := os.Create(path)
+				So(err, ShouldBeNil)
+
+				_, err = f.Write(make([]byte, stats.length))
+				So(err, ShouldBeNil)
+
+				err = f.Close()
+				So(err, ShouldBeNil)
+			} else {
+				err := os.MkdirAll(path, 0755)
+				So(err, ShouldBeNil)
+			}
+
+			stat, err := os.Stat(path)
+			So(err, ShouldBeNil)
+
+			statt, ok := stat.Sys().(*syscall.Stat_t)
+			So(ok, ShouldBeTrue)
+
+			inodes = append(inodes, statt.Ino)
+			dev = statt.Dev
+			atimes = append(atimes, statt.Atim.Sec)
+			ctimes = append(ctimes, statt.Ctim.Sec)
+
+			err = os.Chtimes(path, time.Time{}, stats.mtime)
+			So(err, ShouldBeNil)
+		}
+
+		statDir := t.TempDir()
+		statFilePath := filepath.Join(statDir, "dir.walk")
+		statFile, err := os.Create(statFilePath)
+		So(err, ShouldBeNil)
+
+		err = fs.WalkDir(os.DirFS(tmp), ".", func(path string, _ fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			_, err = io.WriteString(statFile, filepath.Join(tmp, path)+"\n")
+			So(err, ShouldBeNil)
+
+			return nil
+		})
+		So(err, ShouldBeNil)
+
+		err = statFile.Close()
+		So(err, ShouldBeNil)
+
+		_, _, _, err = runWRStat("stat", statFilePath)
+
+		So(err, ShouldBeNil)
+
+		u, err := user.Current()
+		So(err, ShouldBeNil)
+
+		expectation := fmt.Sprintf(""+
+			"%[3]s\t4096\t%[1]s\t%[2]s\t%[14]d\t271828\t%[19]d\td\t%[8]d\t4\t%[13]d\n"+
+			"%[4]s\t4096\t%[1]s\t%[2]s\t%[15]d\t133032\t%[20]d\td\t%[9]d\t3\t%[13]d\n"+
+			"%[5]s\t10\t%[1]s\t%[2]s\t%[16]d\t7383773\t%[21]d\tf\t%[10]d\t1\t%[13]d\n"+
+			"%[6]s\t4096\t%[1]s\t%[2]s\t%[17]d\t314159\t%[22]d\td\t%[11]d\t2\t%[13]d\n"+
+			"%[7]s\t4096\t%[1]s\t%[2]s\t%[18]d\t282820\t%[23]d\td\t%[12]d\t2\t%[13]d\n",
+			u.Uid,
+			u.Gid,
+			base64.StdEncoding.EncodeToString([]byte(tmp)),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aFile"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "aDirectory", "aSubDirectory"))),
+			base64.StdEncoding.EncodeToString([]byte(filepath.Join(tmp, "anotherDirectory"))),
+			inodes[4],
+			inodes[2],
+			inodes[0],
+			inodes[1],
+			inodes[3],
+			dev,
+			atimes[4],
+			atimes[3],
+			atimes[2],
+			atimes[0],
+			atimes[1],
+			ctimes[4],
+			ctimes[3],
+			ctimes[2],
+			ctimes[0],
+			ctimes[1],
+		)
+
+		f, err := os.Open(filepath.Join(statDir, "dir.walk.stats"))
+		So(err, ShouldBeNil)
+
+		data, err := io.ReadAll(f)
+		f.Close()
+		So(err, ShouldBeNil)
+
+		So(string(data), ShouldEqual, expectation)
+	})
+}
+
 func TestMergsDBs(t *testing.T) {
 	Convey("For the mergedbs subcommand, it copies and delete the correct files", t, func() {
 		srcDir := t.TempDir()
@@ -810,6 +808,8 @@ func TestMergsDBs(t *testing.T) {
 	})
 }
 
+func TestBasedirs(t *testing.T) {}
+
 func TestTidy(t *testing.T) {
 	Convey("For the tidy command, combine files within the source directory are cleaned up and moved to the final directory", t, func() {
 		srcDir := t.TempDir()
@@ -874,8 +874,4 @@ func TestTidy(t *testing.T) {
 			So(string(contents), ShouldEqual, expected)
 		}
 	})
-}
-
-func TestBasedirs(t *testing.T) {
-
 }
