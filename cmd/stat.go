3da7d9eeca83fe7fc232d699a54c97a0d4a8d60d
Michael Woolnough <130465766+mjkw31@users.noreply.github.com>
2025-03-11 09:10:47 +0000

    Add byte size to stats in addition to the byte-or-block size added before (#141)
    * Add byte size to stats in addition to the byte-or-block size added before
    
    * Delint'd
    
    * Rename ASize fields to ApparentSize
    
    * Add comment to make it clear that field 12 is always apparent size in bytes)

--- cmd/stat.go
+++ cmd/stat.go
@@ -84,6 +84,7 @@ The output file format is 11 tab separated columns with the following contents:
 9. Inode number (on unix).
 10. Number of hard links.
 11. Identifier of the device on which this file resides.
+12. Apparent file size in bytes (ignores the -b flag).
 
 If you supply a tsv file to --ch with the following columns:
 directory user group fileperms dirperms
